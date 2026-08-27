e421ba81bfa0bbfe52d704fcd5127460590856ce
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-27 23:21:40 +0300

    Preserve toolchain paths in development containers

--- make.generated.mk
+++ make.generated.mk
@@ -26,12 +26,13 @@ LANG_TOOL_TARGETS :=
 LANG_HOST_PREP_TARGETS :=
 LANG_DOCKER_BUILD_TARGETS :=
 LANG_DOCKER_DEV_BUILD_TARGETS :=
+LANG_DOCKER_VERIFY_TARGETS :=
 LANG_INTEGRATION_TARGETS :=
 include make.typescript.generated.mk
 
 DEBUG_TARGETS := debug-analyticsservice debug-automationservice debug-inventoryservice debug-orderservice
 DEPENDENCY_DOCKER_TARGETS := $(LANG_DOCKER_BUILD_TARGETS) $(LANG_DOCKER_DEV_BUILD_TARGETS) \
-	$(DEBUG_TARGETS) docker-build docker-build-local docker-build-dev docker-up docker-up-dev \
+	$(LANG_DOCKER_VERIFY_TARGETS) $(DEBUG_TARGETS) docker-build docker-build-local docker-build-dev docker-up docker-up-dev \
 	kubernetes-up kubernetes-build kubernetes-deploy
 DEPENDENCY_HOST_TARGETS := $(LANG_HOST_PREP_TARGETS)
 include dependency-proxy.generated.mk
