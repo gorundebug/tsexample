079816af2e3d72d7876cffa0fbfed01bfbe67a2e
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-29 09:40:52 +0300

    Regenerate for framework release v0.2.30

--- make.generated.mk
+++ make.generated.mk
@@ -6,7 +6,7 @@ PROJECT_DIR := $(abspath .)
 BIN_DIR := $(PROJECT_DIR)/bin
 TOOLS_DIR := $(PROJECT_DIR)/tools
 GOPRIVATE := github.com
-MODULE_VERSION := v0.2.29
+MODULE_VERSION := v0.2.30
 USE_LOCAL_MODULES ?= 1
 OS := $(shell uname -s)
 ARCH := $(shell uname -m)
@@ -48,7 +48,7 @@ DEPENDENCY_HOST_TARGETS := $(LANG_HOST_PREP_TARGETS)
 include dependency-proxy.generated.mk
 
 ifneq ($(strip $(DEPENDENCY_PROXY_DIR)),)
-export TSSERVICELIB_SOURCE_CONTEXT ?= $(DEPENDENCY_PROXY_DOCKER_BASE)/github-raw/gorundebug/tsservicelib/archive/refs/tags/v0.2.29.tar.gz
+export TSSERVICELIB_SOURCE_CONTEXT ?= $(DEPENDENCY_PROXY_DOCKER_BASE)/github-raw/gorundebug/tsservicelib/archive/refs/tags/v0.2.30.tar.gz
 endif
 
 export DOCKER_TARGET := runtime
